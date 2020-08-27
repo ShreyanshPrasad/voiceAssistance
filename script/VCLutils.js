ded9fc2547142a8db9e7bb1befe2fa1b7352bfac
Shreyansh <sshreyansh199@gmail.com>
2020-08-27 23:12:46 +0530

    theme

--- script/VCLutils.js
+++ script/VCLutils.js
@@ -1,15 +1,25 @@
 window.addEventListener('load', function () {
     
     let tipsArr = ['Welcome to VCL v1.0','<br>',
-            'For time use command \'Whats the time now\' or simply \'time now\'','<br>',
-            'For date use command \'Whats the date today\' or simply \'date today\'','<br>',
+            'Try \'Change Theme\'','<br>',
+            'Try \'Time now\'','<br>',
+            'Try \'Date today\'','<br>',
             'Try \'how are you\'', '<br>',
             'Try \'What is this\'', '<br>',
             'Try \'Who made this\'', '<br>',
             'Try \'I like this project\'', '<br>',
             'Try \'Location\'', '<br>',
+            'Try \'Time Zone\'', '<br>',
             'Try \'Temperature\'', '<br>',
             'Try \'Sunrise and Sunset\'', '<br>',
+            'Try \'Atmospheric pressure\'', '<br>',
+            'Try \'Humidity\'', '<br>',
+            'Try \'Dew point\'', '<br>',
+            'Try \'Clouds\'', '<br>',
+            'Try \'UV index\'', '<br>',
+            'Try \'Visibility\'', '<br>',
+            'Try \'Wind\'', '<br>',
+            'Try \'Weather\'', '<br>',
             'For any help use command \'help me\'','<br><br>'];
     
     let cmd = document.querySelector('.cmdtxt');
--- script/app.js
+++ script/app.js
@@ -14,9 +14,15 @@ let visibility ;
 let wind_speed;
 let wind_deg;
 let weather;
+let weatherMain;
+let weatherDesc;
+const themes = ['dark', 'light', 'default'];
 
 
 window.addEventListener('load', () => {
+    document.body.style.background = 'linear-gradient(141deg, #9fb8ad 0%, #1fc8db 51%)';
+    document.body.style.backgroundSize = '100vw 100vh';
+    document.body.style.backgroundAttachment = 'fixed';
 
     if (navigator.geolocation){
         navigator.geolocation.getCurrentPosition(position => {
@@ -39,6 +45,9 @@ window.addEventListener('load', () => {
                     [sunrise, sunset, temp, pressure, humidity, dew_point, clouds, uvi, visibility, wind_speed, wind_deg] = [data.current.sunrise, data.current.sunset, data.current.temp, data.current.pressure, data.current.humidity, data.current.dew_point, data.current.clouds, data.current.uvi, data.current.visibility, data.current.wind_speed, data.current.wind_deg];
 
                     weather = data.current.weather;
+                    weatherMain = weather[0].main;
+                    weatherDesc = weather[0].description;
+                    console.log(weather);
 
                 })
         });
@@ -49,8 +58,6 @@ window.addEventListener('load', () => {
     const btn = document.querySelector('#btn');
     const clr = document.querySelector('#clr');
     const tips = document.querySelector('.tips');
-
-    const commands = ['dark mode', 'light mode', 'default theme'];
 
     const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
     const recognition = new SpeechRecognition();
@@ -63,7 +70,7 @@ window.addEventListener('load', () => {
         const current = event.resultIndex;
         const transcript = event.results[current][0].transcript;
 
-        tips.innerHTML += '<br>' + transcript + '<br>';
+        tips.innerHTML += '<br>$ ' + transcript + ' ↵ <br>';
         var result = execute(transcript);
         tips.innerHTML += result + '<br>';
         if(transcript.includes('I like this'))
@@ -110,21 +117,56 @@ function execute(command){
     if(command.includes('I like this'))
         return 'If you like this project, dont forget to give a Star @ <a class="link" src="https://github.com/ShreyanshPrasad/voiceAssistance">https://github.com/ShreyanshPrasad/voiceAssistance</a>';
     if(command.includes('weather'))
-        return 'Weather'
+        return `${weatherMain} - ${weatherDesc} <img width="25px" height="25px" src="http://openweathermap.org/img/wn/${weather[0].icon}@2x.png" />`;
     if(command.includes('location'))
         return `Latitude : ${lat}, Longitude : ${lon}`;
     if(command.includes('time zone'))
         return timezone;
     if(command.includes('temperature'))
-        return `${temp} celsius`;
+        return `${temp} degree celsius`;
     if(command.includes('sunrise and sunset') || command.includes('sunrise') || command.includes('sunset'))
-    return `Sunrise at ${changeTime(new Date(sunrise))} and Sunset at ${changeTime(new Date(sunset))}`;
+        return `Sunrise at ${changeTime(new Date(sunrise))} and Sunset at ${changeTime(new Date(sunset))}`;
+    if(command.includes('theme'))
+        return changeTheme();
+    if(command.includes('atmospheric pressure'))
+        return `${pressure} Hectopascal Pressure Unit on the sea level`;
+    if(command.includes('humidity'))
+        return `${humidity}%`;
+    if(command.includes('Dew Point'))
+        return `${dew_point} kelvin`;
+    if(command.includes('clouds'))
+        return `${clouds} %`;
+    if(command.includes('UV index'))
+        return `${uvi}`;
+    if(command.includes('visibility'))
+        return `${visibility} meters`;
+    if(command.includes('wind'))
+        return `${wind_speed} metre per second in ${wind_deg} degrees`;
+    
 }
 
-function changeTheme (theme) {
-    if(theme.includes('dark mode')){
-        document.bgColor = 'black';
+let themeKey = 0;
+
+function changeTheme () {
+
+    if(themeKey == 0){
+        setTheme('light');
+        themeKey++;
+        return 'Enjoy light mode';
     }
+
+    if(themeKey == 1){
+        setTheme('dark');
+        themeKey++;
+        return 'Enjoy dark mode';
+    }
+
+    if(themeKey == 2){
+        setTheme('default');
+        themeKey = 0;
+        return 'Enjoy Default theme';
+    }
+    
 }
 
 function changeDate(obj){
@@ -138,3 +180,43 @@ function changeDate(obj){
 function changeTime(obj){
     return obj.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true });
 }
+
+function setTheme(theme){
+
+    if(theme == 'dark'){
+        document.body.style.background = 'black';
+        document.querySelector('.cmdline').style.background = 'black';
+        document.querySelector('.cmdline').style.color = 'lime';
+        setCookie("userthemechoice", 'dark', 365);
+    }
+
+    if(theme == 'light'){
+        document.body.style.background = 'white';
+        document.querySelector('.cmdline').style.background = 'white';
+        document.querySelector('.cmdline').style.color = 'black';
+        setCookie("userthemechoice", 'light', 365);
+    }
+
+    if(theme == 'default'){
+        document.body.style.background = 'linear-gradient(141deg, #9fb8ad 0%, #1fc8db 51%)';
+        document.querySelector('.cmdline').style.background = 'black';
+        document.querySelector('.cmdline').style.color = 'lime';
+        setCookie("userthemechoice", 'default', 365);
+    }
+}
+
+function checkCookie() {
+    var userPreference = getCookie("userthemechoice");
+    if (userPreference != "") {
+        setTheme(userPreference);
+    } else {
+        setCookie("userthemechoice", themes[themeKey], 30);
+    }
+  }
+
+  function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires=" + d.toGMTString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  }
